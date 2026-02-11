dc7924951b0eeada7395f624bbf79955cc562d93
gdbmrm <mgoudiaby057@gmail.com>
2026-02-11 12:45:57 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Serveur fonctionne");
+});
+
+app.listen(3000, () => {
+  console.log("Serveur lancé sur le port 3000");
+});
